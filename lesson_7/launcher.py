aac05f2a3fce8692d30249d53ebe2f8652185bdc
su <root@root.com>
2021-10-16 01:23:01 +0300

    add launcher to lesson_7

---
+++ lesson_7/launcher.py
@@ -0,0 +1,7 @@
+import subprocess
+subprocess.Popen(['gnome-terminal', '--', 'bash', '-c',
+                  '/usr/bin/python3 /home/su/Projects/client-server-apps/lesson_1/bbb.py'])
+subprocess.Popen(['gnome-terminal', '--', 'bash', '-c',
+                  '/usr/bin/python3 /home/su/Projects/client-server-apps/lesson_1/bbb.py'])
+subprocess.Popen(['gnome-terminal', '--', 'bash', '-c',
+                  '/usr/bin/python3 /home/su/Projects/client-server-apps/lesson_1/bbb.py'])
