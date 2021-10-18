9601ea46d369e9787a16bc6d95b5da9169492f56
su <root@root.com>
2021-10-18 03:10:37 +0300

    fix lesson 2 json

--- lesson_2/practice/3/main_2.py
+++ lesson_2/practice/3/main_2.py
@@ -19,7 +19,7 @@ import yaml
 
 DATA_IN = {'items': ['computer', 'printer', 'keyboard', 'mouse'],
            'items_quantity': 4,
-           'items_ptice': {'computer': '200\u20ac-1000\u20ac',
+           'items_price': {'computer': '200\u20ac-1000\u20ac',
                            'printer': '100\u20ac-300\u20ac',
                            'keyboard': '5\u20ac-50\u20ac',
                            'mouse': '4\u20ac-7\u20ac'}
