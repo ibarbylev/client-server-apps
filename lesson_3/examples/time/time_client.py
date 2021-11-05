c09e14060b43c02ee5f29612f9566bc11bdeaaba
ibarbylev <ibarbylev@gmail.com>
2021-11-05 16:03:33 +0200

    add lesson_7

--- lesson_3/examples/time/time_client.py
+++ lesson_3/examples/time/time_client.py
@@ -5,5 +5,5 @@ from socket import socket, AF_INET, SOCK_STREAM
 CLIENT_SOCK = socket(AF_INET, SOCK_STREAM)
 CLIENT_SOCK.connect(('localhost', 8888))
 TIME_BYTES = CLIENT_SOCK.recv(1024)
-CLIENT_SOCK.close()
 print(f"Текущее время: {TIME_BYTES.decode('utf-8')}")
+CLIENT_SOCK.close()
