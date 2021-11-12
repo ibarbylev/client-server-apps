94e2f15e81c823a994d7fed677e02704db8971fd
su <ibarbylev@gmail.com>
2021-11-12 18:29:29 +0200

    fix lesson_1

--- lesson_1/examples/file_system_5.py
+++ lesson_1/examples/file_system_5.py
@@ -10,9 +10,10 @@ print(type(F_N))
 with open('test.txt', encoding='utf-8') as f_n:
     for el_str in f_n:
         print(el_str, end='')
+    print()
 
 
 # # определение кодировки по умолчанию
-# import locale
-# default_encoding = locale.getpreferredencoding()
-# print(default_encoding)
+import locale
+default_encoding = locale.getpreferredencoding()
+print(default_encoding)
