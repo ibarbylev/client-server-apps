a8264029ee0d035f13af58d56b6dc3e9e262919d
su <ibarbylev@gmail.com>
2021-11-16 18:10:17 +0200

    fix lesson 1

--- lesson_1/practice/task_5_1.py
+++ lesson_1/practice/task_5_1.py
@@ -3,13 +3,15 @@
 преобразовать результаты из байтовового в строковый тип на кириллице.
 """
 
+import platform
 import subprocess
 from chardet import detect
 
 URLS = ['yandex.ru', 'youtube.com']
+CODE = '-n' if platform.system() == 'Windows' else '-c'
 
 for url in URLS:
-    args = ['ping', '-c', '4', url]
+    args = ['ping', CODE, '4', url]
     YA_PING = subprocess.Popen(args, stdout=subprocess.PIPE)
     for line in YA_PING.stdout:
         result = detect(line)
