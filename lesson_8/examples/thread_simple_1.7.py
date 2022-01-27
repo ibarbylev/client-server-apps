afd52e36b888b7cd34c0d119c3767c43c26dd90c
su <root@root.com>
2022-01-27 14:40:08 +0200

    fix lesson 8

--- lesson_8/examples/thread_simple_1.7.py
+++ lesson_8/examples/thread_simple_1.7.py
@@ -7,7 +7,7 @@ import time
 from threading import Thread
 
 
-def create_list(min_index, max_index):
+def calculate_amount(min_index, max_index):
     """Функция, которая может быть запущена в потоке"""
 
     amount = 0
@@ -15,16 +15,16 @@ def create_list(min_index, max_index):
         amount += (x * x) ** x
 
 
-THR1 = Thread(target=create_list, args=(0, 5000))
-THR2 = Thread(target=create_list, args=(5000, 10000))
-THR1.daemon = True
-THR2.daemon = True
+# THR1 = Thread(target=calculate_amount, args=(0, 5000))
+# THR2 = Thread(target=calculate_amount, args=(5000, 10000))
+# THR1.daemon = True
+# THR2.daemon = True
 
 
 print(f"Время запуска основной программы: {time.ctime()}")
 time1 = time.time()
-create_list(0, 5000)
-create_list(5000, 10000)
+calculate_amount(0, 5000)
+calculate_amount(5000, 10000)
 time2 = time.time()
 print(f'total = {time2 - time1:.2f}')
 
--- lesson_8/examples/thread_simple_1.8.py
+++ lesson_8/examples/thread_simple_1.8.py
@@ -7,7 +7,7 @@ import time
 from threading import Thread
 
 
-def create_list(min_index, max_index):
+def calculate_amount(min_index, max_index):
     """Функция, которая может быть запущена в потоке"""
 
     amount = 0
@@ -15,8 +15,8 @@ def create_list(min_index, max_index):
         amount += (x * x) ** x
 
 
-THR1 = Thread(target=create_list, args=(0, 5000))
-THR2 = Thread(target=create_list, args=(5000, 10000))
+THR1 = Thread(target=calculate_amount, args=(0, 5000))
+THR2 = Thread(target=calculate_amount, args=(5000, 10000))
 THR1.daemon = True
 THR2.daemon = True
 
@@ -29,6 +29,6 @@ THR2.start()
 THR1.join()
 THR2.join()
 time2 = time.time()
-print(f'first part = {time2 - time1:.2f}')
+print(f'total = {time2 - time1:.2f}')
 
 
