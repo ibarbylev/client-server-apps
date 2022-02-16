69ef594b056775c0479a0b0da648c5ec8354ed28
su <root@root.com>
2022-02-16 23:50:07 +0200

    fix lesson 3

--- lesson_3/practice/common/utils.py
+++ lesson_3/practice/common/utils.py
@@ -29,7 +29,8 @@ def send_message(sock, message):
     :param message:
     :return:
     '''
-
+    if not isinstance(message, dict):
+        raise TypeError
     js_message = json.dumps(message)
     encoded_message = js_message.encode(ENCODING)
     sock.send(encoded_message)
