9950193dae53947a3244d0cf8e139be0a7529b98
su <root@root.com>
2022-02-22 21:04:32 +0200

    fix lesson 1

--- lesson_1/examples/modules_4.py
+++ lesson_1/examples/modules_4.py
@@ -9,8 +9,8 @@ import platform
 
 param = '-n' if platform.system().lower() == 'windows' else '-c'
 args = ['ping', param, '2', 'yandex.ru']
-result = subprocess.Popen(args, stdout=subprocess.PIPE)
-for line in result.stdout:
+process = subprocess.Popen(args, stdout=subprocess.PIPE)
+for line in process.stdout:
     result = chardet.detect(line)
     print('result = ', result)
     line = line.decode(result['encoding']).encode('utf-8')
