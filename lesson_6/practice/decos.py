3ca50d193ed4a5f09b54a88e7cf501b57d48f5fb
ibarbyler <ibarbylev@gmail.com>
2022-02-27 22:10:58 +0200

    change thr findinf of LOGGER in decos

--- lesson_6/practice/decos.py
+++ lesson_6/practice/decos.py
@@ -11,17 +11,18 @@ import inspect
 # Метод find () возвращает индекс первого вхождения искомой подстроки,
 # если он найден в данной строке.
 # Если его не найдено, - возвращает -1.
-if sys.argv[0].find('client.py') == -1:
-    # если не клиент то сервер!
-    LOGGER = logging.getLogger('server')
-else:
-    # ну, раз не сервер, то клиент
-    LOGGER = logging.getLogger('client')
 
 
 def log(func_to_log):
     """Функция-декоратор"""
     def log_saver(*args, **kwargs):
+        if sys.argv[0].find('client.py') == -1:
+            # если не клиент то сервер!
+            LOGGER = logging.getLogger('server')
+        else:
+            # ну, раз не сервер, то клиент
+            LOGGER = logging.getLogger('client')
+
         ret = func_to_log(*args, **kwargs)
         LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}.'
                      f'Вызов из модуля {func_to_log.__module__}.'
