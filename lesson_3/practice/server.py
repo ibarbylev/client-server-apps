d2272b734bc1326daace14451c4f0c142ec32fbc
ibarbylev <ibarbylev@gmail.com>
2022-10-06 16:16:59 +0300

    some fixes

--- lesson_3/practice/server.py
+++ lesson_3/practice/server.py
@@ -13,7 +13,9 @@ def process_client_message(message):
     сообщение от клиента, проверяет корректность,
     возвращает словарь-ответ для клиента
     """
-    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+    if ACTION in message \
+            and message[ACTION] == PRESENCE \
+            and TIME in message \
             and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
         return {RESPONSE: 200}
     return {
