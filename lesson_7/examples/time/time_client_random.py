6a77406faf47f3fb188649e191bb4736a624e8dd
ibarbylev <ibarbylev@gmail.com>
2022-10-18 15:38:12 +0300

    change examples of lesson 7

--- lesson_7/examples/time/time_client_random.py
+++ lesson_7/examples/time/time_client_random.py
@@ -1,14 +1,11 @@
-"""
-Программа клиента, бесконечно запрашивающего текущее время
-"""
+"""Client program that endlessly requests the current time"""
 
 from socket import socket, AF_INET, SOCK_STREAM
 
-SOCK = socket(AF_INET, SOCK_STREAM)
-SOCK.connect(('localhost', 8888))
+with socket(AF_INET, SOCK_STREAM) as client_sock:
+    client_sock.connect(('localhost', 8888))
 
-while True:
-    TIME_BYTES = SOCK.recv(1024)
-    print(f"Текущее время: {TIME_BYTES.decode('utf-8')}")
+    while True:
+        time_bytes = client_sock.recv(1024)
+        print(f"Текущее время: {time_bytes.decode('utf-8')}")
 
-SOCK.close()
--- lesson_7/examples/time/time_server_select.py
+++ lesson_7/examples/time/time_server_select.py
@@ -1,6 +1,5 @@
-"""
-Сервер времени, обрабатывающий "одновременно" несколько клиентов
-Обработка клиентов осуществляется функцией select
+"""A time server that handles "simultaneously" multiple clients.
+Client processing is done by the select function
 """
 
 import time
@@ -9,7 +8,7 @@ from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 
 
 def new_listen_socket(address):
-    """Инициируем серверный сокет"""
+    """Initiating a server socket"""
     sock = socket(AF_INET, SOCK_STREAM)
     sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     sock.bind(address)
@@ -21,27 +20,23 @@ def new_listen_socket(address):
 
 
 def mainloop():
-    """Основной цикл обработки запросов клиентов"""
+    """Main client request processing loop"""
     address = ('', 8888)
     all_clients = []
     sock = new_listen_socket(address)
 
     while True:
         try:
-            # Проверка подключений
             conn, addr = sock.accept()
-            print(conn)
-        # timeout вышел
         except OSError as err:
             print(f'OSError code on the server side is : {err.errno}')
             # The error number returns None because it's just a timeout
             pass
         else:
-            print(f"Получен запрос на соединение с {str(addr)}")
-            # вносим в список добавившегося клиента
+            print(f"Received a connection request from {str(addr)}")
+            # Add a new client to the list all_clients
             all_clients.append(conn)
         finally:
-
             clients_write = []
             try:
                 _, clients_write, _ = select.select([], all_clients, [], 0)
@@ -51,18 +46,18 @@ def mainloop():
                 pass
 
             for client in clients_write:
-                # определяем время
+                # get time value
                 time_str = time.ctime(time.time()) + "\n"
                 try:
-                    # отправляем время клиенту
+                    # and sent time value to client
                     client.send(time_str.encode('utf-8'))
 
                 except Exception as e:
                     print(e)
-                    # клиент отключился
+                    # client disconnected
                     all_clients.remove(client)
                     client.close()
 
 
-print('Эхо-сервер запущен!')
+print('Echo server started...')
 mainloop()
