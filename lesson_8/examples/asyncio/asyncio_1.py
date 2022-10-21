1c8805d56424a151647f071b8c5e8c9a7f104751
ibarbylev <ibarbylev@gmail.com>
2022-10-21 06:09:31 +0300

    fix example for lesson 8

--- lesson_8/examples/asyncio/asyncio_1.py
+++ lesson_8/examples/asyncio/asyncio_1.py
@@ -1,22 +1,31 @@
+"""Task: create output for each pair of values from functions
+function1 and function2:
+v1 --- v2
+"""
+import random
+
+
 def func1():
     for x in range(10**10000):
-        yield x
+        v1 = random.randint(0, x)
+        yield v1
 
 
 def func2():
     for x in range(10**10000):
-        yield x * (-1)
+        v2 = random.randint(0, x)
+        yield v2
 
 
 def main():
-    f1 = func1()
-    f2 = func2()
-    try:
-        while True:
-            print(next(f1), end=' -- ')
-            print(next(f2))
-    except Exception as e:
-        print(e)
+    v1 = func1()
+    v2 = func2()
+    while True:
+        try:
+            print(next(v1), end=' -- ')
+            print(next(v2))
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
